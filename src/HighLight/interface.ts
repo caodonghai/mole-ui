2e727ae07064d3066ac71e8c44b3533833dac1a6
人生海海 <caodonghai234@163.com>
2025-12-30 17:23:36 +0800

    接口定义规范化

--- src/HighLight/interface.ts
+++ src/HighLight/interface.ts
@@ -1,10 +1,25 @@
 export interface IHighLightContext {
+  /**
+   * 需要高亮的文本
+   */
   highLightText: string;
+  /**
+   * 高亮文本的颜色
+   */
   color: string;
+  /**
+   * 高亮文本的处理函数
+   */
   handler?: Parameters<typeof String.prototype.replace>[1];
 }
 
 export interface IHighLightProps {
+  /**
+   * 需要处理高亮的原始文本
+   */
   text: string;
+  /**
+   * 高亮的文本和颜色配置
+   */
   highLightContext: IHighLightContext | IHighLightContext[];
 }
